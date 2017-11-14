71cb7e49a5d4ac0d627377242effc5867854a98b
Bernardo Vallejo <b.vallejo@alumnos.upm.es>
2017-11-14 04:47:35 +0100

    Nothing

--- Ram_to_Ram_PB/Ram_to_Ram_PB.sim/sim_1/impl/func/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\XilinxVivado\\Vivado\\2017.2\\bin
-call %xv_path%/xsim Main_func_impl -key {Post-Implementation:sim_1:Functional:Main} -tclbatch Main.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
