6b979f9accf0511eb980fd93a504412d1b317c42
Bernardo Vallejo <b.vallejo@alumnos.upm.es>
2017-11-14 05:12:35 +0100

    Advanced Embedded System
    Transfer between memories

---
+++ lab4aes/lab4aes.runs/synth_1/.Xil/system_wrapper_propImpl.xdc
@@ -0,0 +1,3 @@
+set_property SRC_FILE_INFO {cfile:C:/xup/fpga_flow/2016_2_zynq_labs/lab4aes/lab4aes.srcs/constrs_1/imports/lab1/lab1_zybo.xdc rfile:../../../lab4aes.srcs/constrs_1/imports/lab1/lab1_zybo.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:4 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN Y16 [get_ports btn_tri_io]
---
+++ lab4aes/lab4aes.sdk/lab4/Debug/src/lab4.d
@@ -0,0 +1,85 @@
+src/lab4.o src/lab4.o: ../src/lab4.c \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xaxicdma.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xaxicdma_bd.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xaxicdma_hw.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab4_bsp/ps7_cortexa9_0/include/xil_types.h
+
+../../lab4_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xaxicdma.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xaxicdma_bd.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xaxicdma_hw.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab4_bsp/ps7_cortexa9_0/include/xil_types.h:
